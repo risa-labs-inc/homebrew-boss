a34320ba1e931179fd4c11324ee4e4b28d9da7bf
kshivang <shivang.iitk@gmail.com>
2025-12-21 09:43:33 +0000

    🚀 Update BOSS to version 8.13.26
      Release: https://github.com/risa-labs-inc/BOSS-Releases/releases/tag/v8.13.26
      SHA256: 1be2022ea1879c10f19fd47724aea63171da6ad4c104d893dd0c932ffd4ec7c8

--- Casks/boss.rb
+++ Casks/boss.rb
@@ -1,6 +1,6 @@
 cask "boss" do
-  version "8.13.23"
-  sha256 "54db12e9460ec4b3dcce82d65d4dc525f8b53d098ecc5d76c47bb19c2d06a349"
+  version "8.13.26"
+  sha256 "1be2022ea1879c10f19fd47724aea63171da6ad4c104d893dd0c932ffd4ec7c8"
 
   url "https://github.com/risa-labs-inc/BOSS-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
       verified: "github.com/risa-labs-inc/BOSS-Releases/"
