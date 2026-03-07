9bde26b185ada4529f843d4774724e88a7df90c6
kshivang <shivang.iitk@gmail.com>
2026-03-07 23:54:16 +0000

    🚀 Update BOSS to version 8.17.18
      Release: https://github.com/risa-labs-inc/BossConsole-Releases/releases/tag/v8.17.18
      SHA256: 74e158d2bb37f423f49c482c69b010abb9a12d173690613279f2d95e8d880e1f

--- Casks/boss.rb
+++ Casks/boss.rb
@@ -1,6 +1,6 @@
 cask "boss" do
-  version "8.17.17"
-  sha256 "4eec880c0a1cf1fd1eac9b543ded567d0d7cbc7e05c827ec5b35f043f57f23c6"
+  version "8.17.18"
+  sha256 "74e158d2bb37f423f49c482c69b010abb9a12d173690613279f2d95e8d880e1f"
 
   url "https://github.com/risa-labs-inc/BossConsole-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
       verified: "github.com/risa-labs-inc/BossConsole-Releases/"
