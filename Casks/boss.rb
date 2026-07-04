38d9f178e8caf96b88229cf3e38dc4b8249b38b2
kshivang <shivang.iitk@gmail.com>
2026-07-04 21:39:03 +0000

    🚀 Update BOSS to version 9.2.32
      Release: https://github.com/risa-labs-inc/BossConsole-Releases/releases/tag/v9.2.32
      SHA256: 244f5a25ffb6f512708c0326ba4b32f52570695f35df21d177c4c6a257416005

--- Casks/boss.rb
+++ Casks/boss.rb
@@ -1,6 +1,6 @@
 cask "boss" do
-  version "9.2.31"
-  sha256 "4230841989619482af1040ac280e6a60601ac23c025e52488c507f524c7d243f"
+  version "9.2.32"
+  sha256 "244f5a25ffb6f512708c0326ba4b32f52570695f35df21d177c4c6a257416005"
 
   url "https://github.com/risa-labs-inc/BossConsole-Releases/releases/download/v#{version}/BOSS-#{version}-Universal.dmg",
       verified: "github.com/risa-labs-inc/BossConsole-Releases/"
